fcc78a3869138d6c618063318fe3ded3d5908dd8
nickhoffmann7 <hoffmannnick97@gmail.com>
2020-03-04 15:42:51 +0100

    Added initial html portfolio list

---
+++ src/main/java/mops/portfolios/Portfolio.java
@@ -0,0 +1,28 @@
+package mops.portfolios;
+
+import lombok.Data;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class Portfolio {
+
+  private String course;
+
+  private List<String> students = new ArrayList<>();
+
+  private long lastChange = System.currentTimeMillis();
+
+  public String getLastChangeDate() {
+    Date date = new Date(lastChange);
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+    return dateFormat.format(date);
+  }
+
+  public Portfolio(String course) {
+    this.course = course;
+  }
+}
