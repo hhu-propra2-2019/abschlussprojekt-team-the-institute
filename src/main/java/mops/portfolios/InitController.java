4aac8195827b6f0899c8dadfe2811e4737f70616
Nico <49519524+Nisch206@users.noreply.github.com>
2020-03-04 15:04:49 +0100

    Fixing gradle (#6)
    * Aufräumen des Controller Codes.
    
    * Von hier sollte es nicht mehr weit sein. ;-)
    
    * Funktionierender Build.

--- src/main/java/mops/portfolios/InitController.java
+++ src/main/java/mops/portfolios/InitController.java
@@ -1,6 +1,5 @@
 package mops.portfolios;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class InitController {
 
-  @Autowired
-  private HelloWorld greeter = new HelloWorld();
+  private transient HelloWorld greeter;
+
+  public InitController(HelloWorld greeter) {
+    this.greeter = greeter;
+  }
 
   @GetMapping("/")
   public String testGreet(Model model) {
