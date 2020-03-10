4b75c7649a0763351ac1f667775691512f5846fd
mkasimd <49522425+mkasimd@users.noreply.github.com>
2020-03-10 16:41:50 +0100

    Modified structure for retrieving data
    * A DatabaseUpdater in a different thread to update local database
    * Using local database to retrieve data
    
    Signed-off-by: mkasimd <49522425+mkasimd@users.noreply.github.com>

---
+++ src/main/java/mops/portfolios/DatabaseUpdater.java
@@ -0,0 +1,65 @@
+package mops.portfolios;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
+@Service
+public class DatabaseUpdater implements Runnable {
+
+  @Override
+  public void run() {
+    updateDatabaseEvents();
+  }
+
+  void updateDatabase() throws InterruptedException {
+    Thread.sleep(10_000);
+    run();
+  }
+
+
+  /**
+   * Use this method to get the updates from Gruppenbildung regarding groups.
+   */
+  private void updateDatabaseEvents() {
+    HttpClient httpClient = new HttpClient();
+    String responseBody = null;
+
+    // try to receive data from service Gruppenbildung
+    try {
+      // TODO: genaues URI mit gruppen2 absprechen
+      responseBody = httpClient.get("/gruppen2/groupmembers");
+    } catch (HttpClientErrorException clientErr) { // if status 4xx or 5xx returned
+      logger.warn("The service Gruppenbildung is not reachable ", clientErr);
+      // keep responseBody null or empty here
+    }
+
+    // if couldn't retrieve data or not modified, keep the current state
+    if (responseBody == null || responseBody.isEmpty()) {
+      // TODO: use data from local database
+      logger.info("Database not modified");
+      return; // no need to update local database
+    }
+
+    // check for possible errors
+    JSONObject jsonObject = null;
+    try {
+      jsonObject = new JSONObject(responseBody);
+    } catch (JSONException jsonErr) {
+      logger.error("An error occured while parsing the JSON data "
+              + "received by the service Gruppenbildung ", jsonErr);
+      // FIXME: keep this only while in development
+      throw new RuntimeException("Error while trying to parse HTTP response to JSON object");
+    }
+    if (jsonObject == null) {
+      // FIXME: Keep this only while in development
+      logger.error("An error occured while parsing the JSON data "
+              + "received by the service Gruppenbildung ", jsonErr);
+      throw new RuntimeException("JSON Object is null");
+    }
+
+    // TODO: Process the received data
+  }
+
+}
--- src/main/java/mops/portfolios/Group.java
+++ src/main/java/mops/portfolios/Group.java
@@ -2,6 +2,8 @@ package mops.portfolios;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -20,38 +22,11 @@ public class Group {
   } //TODO
 
   /**
-   * Use this method to get the updates from Gruppenbildung regarding groups.
+   * Use this method to retrieve users in a Group.
    * @param userId The user ID of the student to get the group members of
    * @return List of the Account of each group member
    */
-  List<String> getGroupmembers(String userId) {
-    HttpClient httpClient = new HttpClient();
-
-    String responseBody = null;
-    try {
-      // TODO: genaues URI mit gruppen2 absprechen
-      responseBody = httpClient.get("/gruppen2/groupmembers");
-    } catch (HttpClientErrorException clientErr) { // if status 4xx or 5xx returned
-      logger.warn("The service Gruppenbildung is not reachable ", clientErr);
-      // keep responseBody null or empty here
-    }
-
-    if (responseBody == null || responseBody.isEmpty()) {
-      // TODO: use data from local database
-      logger.info("Database not modified");
-      return getFromLocalDatabase(userId);
-    }
-
-    JSONObject jsonObject;
-    try {
-      jsonObject = new JSONObject(responseBody);
-    } catch (JSONException jsonErr) {
-      logger.error("An error occured while parsing the JSON data "
-              + "received by the service Gruppenbildung ", jsonErr);
-      // FIXME: keep this only while in development
-      throw new RuntimeException("Error while trying to parse HTTP response to JSON object");
-    } // TODO once we get data from gruppen2
-
+  List<String> getGroupMembers(String userId) {
 
     List<String> accountList = new ArrayList<>();
     return accountList;
@@ -61,5 +36,6 @@ public class Group {
     // TODO List<Group> groups = findGroupsByUserId(userId);
     return null; // FIXME this is a filler
   }
+
 
 }
