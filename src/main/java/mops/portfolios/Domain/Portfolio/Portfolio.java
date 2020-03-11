368cd98ca4f8b4f06fdc2f05d0b9e4d61908c991
Dmitrii <ostasevich@gmail.com>
2020-03-11 16:16:34 +0100

    Cherry-pick single file
    Signed-off-by: Dmitrii <ostasevich@gmail.com>

--- src/main/java/mops/portfolios/Domain/Portfolio/Portfolio.java
+++ src/main/java/mops/portfolios/Domain/Portfolio/Portfolio.java
@@ -1,27 +1,66 @@
-package mops.portfolios.Domain.Portfolio;
+package mops.portfolios.Portfolio;
 
 import lombok.*;
+import mops.portfolios.Entry.Entry;
 
 import javax.persistence.*;
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@RequiredArgsConstructor
-@NoArgsConstructor
 public class Portfolio {
     private @Id @GeneratedValue @Getter Long id;
 
-    private @NonNull @Column(nullable = false) @Getter @Setter String title;
+    private @Column(nullable = false) @Getter @Setter String title;
 
-    private @Getter UUID accountId;
+    private @Getter String userId;
 
     private @Getter Long groupId;
 
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Entry> entries = new ArrayList<>();
+
+    public Portfolio(){};
+
+    public Portfolio(String title, User user)
+    {
+        this.title = title;
+        this.userId = user.getId();
+    }
+
+    public Portfolio(String title, Group group)
+    {
+        this.title = title;
+        this.groupId = group.getId();
+    }
+
+}
+
+// FIXME: Dummy
+class User {
+    private @Getter String id;
+
+    User(String id) {
+        this.id = id;
+    }
+}
+
+// FIXME: Dummy
+class Group {
+    private @Getter Long id;
+
+    Group(Long id) {
+        this.id = id;
+    }
 }
 
 class Test {
-    Test() {
-        Portfolio p = new Portfolio("Title", UUID.randomUUID(), null);
-        Portfolio p2 = new Portfolio("Title2", null, null);
-    }
+    static User u = new User("USER_ID_DUMMY");
+    static Group g = new Group(999L);
+    static Portfolio userPortfolio = new Portfolio("Title here", u);
+    static Portfolio groupPortfolio = new Portfolio("Title here", g);
+    static String i = userPortfolio.getUserId();
 }
