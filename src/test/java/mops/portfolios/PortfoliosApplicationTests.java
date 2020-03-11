05de5da96b5387ad6d742351461f354f2b65dcbe
nickhoffmann7 <hoffmannnick97@gmail.com>
2020-03-11 16:20:23 +0100

    Changed status code 200 to 302

--- src/test/java/mops/portfolios/PortfoliosApplicationTests.java
+++ src/test/java/mops/portfolios/PortfoliosApplicationTests.java
@@ -18,6 +18,6 @@ class PortfoliosApplicationTests {
 
   @Test
   void contextLoads() throws Exception {
-    mockMvc.perform(get("/")).andExpect(MockMvcResultMatchers.status().is(200));
+    mockMvc.perform(get("/")).andExpect(MockMvcResultMatchers.status().is(302));
   }
 }
