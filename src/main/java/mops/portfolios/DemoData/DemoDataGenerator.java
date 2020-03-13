c567ae8b64ccce433cc5b949a939e341f9564975
allap103 <allap103@hhu.de>
2020-03-13 16:34:04 +0100

    Bug in der DemoDataGenerator-Klasse behoben

--- src/main/java/mops/portfolios/DemoData/DemoDataGenerator.java
+++ src/main/java/mops/portfolios/DemoData/DemoDataGenerator.java
@@ -18,73 +18,73 @@ import java.util.stream.IntStream;
 
 public class DemoDataGenerator {
 
-//    private final Faker faker = new Faker(Locale.GERMAN);
-//
-//    private EntryField generateEntryField(Entry entry) {
-//        EntryField entryField = new EntryField();
-//        entryField.setAttachment(faker.shakespeare().hamletQuote());
-//        entryField.setContent(faker.shakespeare().kingRichardIIIQuote());
-//        entryField.setEntryFieldTitle(faker.shakespeare().romeoAndJulietQuote());
-//        entryField.setEntry(entry);
-//        return entryField;
-//    }
-//
-//    private List<EntryField> generateEntryFieldList(Entry entry) {
-//        return IntStream.range(0, 3).mapToObj(
-//                value -> generateEntryField(entry)).collect(Collectors.toList());
-//    }
-//
-//    private Entry generateGroupEntry(){
-//        Entry entry = new Entry();
-//        entry.setEntryTitle(faker.shakespeare().romeoAndJulietQuote());
-//        entry.getFields().addAll(generateEntryFieldList(entry));
-//        return entry;
-//    }
-//
-//    private List<Entry> generateGroupEntryList() {
-//        return IntStream.range(0, 3).mapToObj(
-//                value -> generateGroupEntry()).collect(Collectors.toList());
-//    }
-//
-//    public Portfolio generateGroupPortfolio() {
-//        Portfolio portfolio = new Portfolio(faker.shakespeare().romeoAndJulietQuote(),generateGroup());
-//        portfolio.getEntries().addAll(generateGroupEntryList());
-//        return portfolio;
-//    }
-//
-//
-//    private Entry generateUserEntry() {
-//        Entry entry = new Entry();
-//        entry.setFields(generateEntryFieldList(entry));
-//        return entry;
-//    }
-//
-//    private List<Entry> generateUserEntryList() {
-//        return IntStream.range(0, 3).mapToObj(
-//                value -> generateUserEntry()).collect(Collectors.toList());
-//    }
-//
-//    public Portfolio generateUserPortfolio() {
-//        Portfolio portfolio = new Portfolio(faker.shakespeare().romeoAndJulietQuote(), generateUser());
-//        portfolio.getEntries().addAll(generateUserEntryList());
-//        return portfolio;
-//    }
-//
-//    private User generateUser() {
-//        HashSet<String> roles = new HashSet<>();
-//        roles.add("role_1");
-//        roles.add("role_2");
-//        roles.add("role_3");
-//        return new User(
-//                "user_name",
-//                "user_mail",
-//                "user_image",
-//                roles,
-//                "userId");
-//    }
-//
-//    private Group generateGroup() {
-//        return new Group(9876543210L, "test");
-//    }
+    private final Faker faker = new Faker(Locale.GERMAN);
 
+    private EntryField generateEntryField(Entry entry) {
+        EntryField entryField = new EntryField();
+        entryField.setAttachment(faker.shakespeare().hamletQuote());
+        entryField.setContent(faker.shakespeare().kingRichardIIIQuote());
+        entryField.setEntryFieldTitle(faker.shakespeare().romeoAndJulietQuote());
+        entryField.setEntry(entry);
+        return entryField;
+    }
+
+    private List<EntryField> generateEntryFieldList(Entry entry) {
+        return IntStream.range(0, 3).mapToObj(
+                value -> generateEntryField(entry)).collect(Collectors.toList());
+    }
+
+    private Entry generateGroupEntry(){
+        Entry entry = new Entry();
+        entry.setEntryTitle(faker.shakespeare().romeoAndJulietQuote());
+        entry.getFields().addAll(generateEntryFieldList(entry));
+        return entry;
+    }
+
+    private List<Entry> generateGroupEntryList() {
+        return IntStream.range(0, 3).mapToObj(
+                value -> generateGroupEntry()).collect(Collectors.toList());
+    }
+
+    public Portfolio generateGroupPortfolio() {
+        Portfolio portfolio = new Portfolio(faker.shakespeare().romeoAndJulietQuote(),generateGroup());
+        portfolio.getEntries().addAll(generateGroupEntryList());
+        return portfolio;
+    }
+
+
+    private Entry generateUserEntry() {
+        Entry entry = new Entry();
+        entry.setEntryTitle(faker.shakespeare().romeoAndJulietQuote());
+        entry.setFields(generateEntryFieldList(entry));
+        return entry;
+    }
+
+    private List<Entry> generateUserEntryList() {
+        return IntStream.range(0, 3).mapToObj(
+                value -> generateUserEntry()).collect(Collectors.toList());
+    }
+
+    public Portfolio generateUserPortfolio() {
+        Portfolio portfolio = new Portfolio(faker.shakespeare().romeoAndJulietQuote(), generateUser());
+        portfolio.getEntries().addAll(generateUserEntryList());
+        return portfolio;
+    }
+
+    private User generateUser() {
+        HashSet<String> roles = new HashSet<>();
+        roles.add("role_1");
+        roles.add("role_2");
+        roles.add("role_3");
+        return new User(
+                "user_name",
+                "user_mail",
+                "user_image",
+                roles,
+                "userId");
+    }
+
+    private Group generateGroup() {
+        return new Group(9876543210L, "test");
+    }
 }
--- src/main/java/mops/portfolios/PortfoliosApplication.java
+++ src/main/java/mops/portfolios/PortfoliosApplication.java
@@ -3,6 +3,8 @@ package mops.portfolios;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import mops.portfolios.DemoData.DemoDataGenerator;
+import mops.portfolios.Domain.Entry.EntryFieldRepository;
+import mops.portfolios.Domain.Entry.EntryRepository;
 import mops.portfolios.Domain.Portfolio.Portfolio;
 import mops.portfolios.Domain.Portfolio.PortfolioRepository;
 import org.slf4j.Logger;
@@ -30,41 +32,42 @@ public class PortfoliosApplication {
     }
 
     @Bean
-    public CommandLineRunner demo(PortfolioRepository repository) {
+    public CommandLineRunner demo(PortfolioRepository PortfolioRepository, EntryRepository entryRepository, EntryFieldRepository entryFieldRepository) {
         return (args) -> {
-//            Set<String> roles = new HashSet<>(Arrays.asList("student"));
-//
-//            User user1 = new User("User1", "mail1@example.com", null, roles, "UUID-1234-5678");
-//            User user2 = new User("User2", "mail2@example.com", null, roles, "UUID-4321-9876");
-//
-//            Group group1 = new Group(123L, "Group1");
-//
-//            UserGroup userGroup1 = new UserGroup(user1.getId(), group1.getId(), "title11111111111");
-//            UserGroup userGroup2 = new UserGroup(user2.getId(), group1.getId(), "title22222222222");
-//
-//            repository.save(userGroup1);
-//            repository.save(userGroup2);
-//
-//            log.info("1==================================================================================");
-//            log.info("RUN ALL THE THINGS!!!!11");
-//            log.info("2==================================================================================");
-//            log.info("3==================================================================================");
-//            log.info("4==================================================================================");
-//            log.info("5==================================================================================");
-//            log.info("6==================================================================================");
-//            log.info("7 - pmd, oh boi======================================================================");
+      /*      Set<String> roles = new HashSet<>(Arrays.asList("student"));
 
-            DemoDataGenerator demo = new DemoDataGenerator();
+            User user1 = new User("User1", "mail1@example.com", null, roles, "UUID-1234-5678");
+            User user2 = new User("User2", "mail2@example.com", null, roles, "UUID-4321-9876");
 
-//            for (int i = 0; i < 10; i++) {
-//                repository.save(demo.generateUserPortfolio());
-//                repository.save(demo.generateGroupPortfolio());
-////            }
-//
-//
-//            for (Portfolio portfolio : repository.findAll()) {
-//                log.info(portfolio.toString());
-//            }
+            Group group1 = new Group(123L, "Group1");
+
+            UserGroup userGroup1 = new UserGroup(user1.getId(), group1.getId(), "title11111111111");
+            UserGroup userGroup2 = new UserGroup(user2.getId(), group1.getId(), "title22222222222");
+
+            repository.save(userGroup1);
+            repository.save(userGroup2);
+
+            log.info("1==================================================================================");
+            log.info("RUN ALL THE THINGS!!!!11");
+            log.info("2==================================================================================");
+            log.info("3==================================================================================");
+            log.info("4==================================================================================");
+            log.info("5==================================================================================");
+            log.info("6==================================================================================");
+            log.info("7 - pmd, oh boi======================================================================");
+
+      */      DemoDataGenerator demo = new DemoDataGenerator();
+
+            for (int i = 0; i < 10; i++) {
+                repository.save(demo.generateUserPortfolio());
+                repository.save(demo.generateGroupPortfolio());
+            }
+
+
+
+            for (Portfolio portfolio : repository.findAll()) {
+                log.info(portfolio.toString());
+            }
 
         };
     }
