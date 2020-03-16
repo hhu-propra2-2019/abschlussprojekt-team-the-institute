297080cd00bc13e516524a767ea560452718e8e4
hanic101 <49514789+hanic101@users.noreply.github.com>
2020-03-16 13:47:05 +0100

    Security#2 (#31)
    * Deleting hasGroupId since it is not needed anymore
    
    Signed-off-by: hanic101 <hannah-nickel@gmx.de>
    
    * Changing application.properties to keycloak clients
    
    Signed-off-by: hanic101 <hannah-nickel@gmx.de>

--- src/main/java/mops/portfolios/UserSecurity.java
+++ src/main/java/mops/portfolios/UserSecurity.java
@@ -8,32 +8,7 @@ public class UserSecurity {
 
   private transient Portfolio portfolio;
 
-  /** Checks if KeycloakUserId equals Portfolio-userId.
-   * @param userId - usedId to check
-   * @return - returns true or false
-   */
-  public boolean hasGroupId(String userId) {
-
-    String userIdOfPortfolio;
-
-    if (portfolio == null) {
-      return false;
-    }
-
-    userIdOfPortfolio = portfolio.getUserId();
-
-    if (userIdOfPortfolio != null && userIdOfPortfolio.equals(userId)) {
-      return true;
-    }
-
-    return false;
-  }
-
-  /** Checks if KeycloakUserId equals Portfolio-userId.
-   * @param userId - usedId to check
-   * @return - returns true or false
-   */
-  public boolean hasUserId(String userId) {
+  public boolean hasUserId (String userId) {
 
     String userIdOfPortfolio;
 
