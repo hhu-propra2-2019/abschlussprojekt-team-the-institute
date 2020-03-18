fb4a19bd857217ae1c714a5f268ff39e21fc3c11
nickhoffmann7 <hoffmannnick97@gmail.com>
2020-03-18 16:38:26 +0100

    added first inputs

--- src/main/java/mops/portfolios/templates/TemplateEntry.java
+++ src/main/java/mops/portfolios/templates/TemplateEntry.java
@@ -12,12 +12,4 @@ public class TemplateEntry {
   private AnswerType type;
 
   private String hint;
-
-  enum AnswerType {
-    TEXT,
-    SINGLE_CHOICE, //hint=choice1,choice2,...
-    MULTIPLE_CHOICE, //hint=choice1,choice2,...
-    NUMBER_SLIDER, //hint=min,max
-    ATTACHEMENT; //hint=.asciidoc,.adoc,.yml,...
-  }
 }
--- src/main/java/mops/portfolios/templates/TemplateService.java
+++ src/main/java/mops/portfolios/templates/TemplateService.java
@@ -11,27 +11,27 @@ public class TemplateService {
     private List<Template> templateList = Arrays.asList(
 
         new Template("Propra1", "Übung_1", Arrays.asList(
-            new TemplateEntry("Was hast du heute gelernt?", TemplateEntry.AnswerType.TEXT, ""),
-            new TemplateEntry("Was hast du nicht verstanden?", TemplateEntry.AnswerType.TEXT, ""),
-            new TemplateEntry("Was könnte man besser machen?", TemplateEntry.AnswerType.MULTIPLE_CHOICE, "Mehr auf Schüler eingehen,Umfangreicher erklären,Weniger Hausaufgaben"),
-            new TemplateEntry("Schreibe eine Java-Klasse..", TemplateEntry.AnswerType.ATTACHEMENT, ".java"),
-            new TemplateEntry("Bewerte die Übung heute", TemplateEntry.AnswerType.NUMBER_SLIDER, "1,10")
+            new TemplateEntry("Was hast du heute gelernt?", AnswerType.TEXT, "Some hint"),
+            new TemplateEntry("Was hast du nicht verstanden?", AnswerType.TEXT, "Some hint"),
+            new TemplateEntry("Was könnte man besser machen?", AnswerType.MULTIPLE_CHOICE, "Mehr auf Schüler eingehen,Umfangreicher erklären,Weniger Hausaufgaben"),
+            new TemplateEntry("Schreibe eine Java-Klasse..", AnswerType.ATTACHEMENT, ".java"),
+            new TemplateEntry("Bewerte die Übung heute", AnswerType.NUMBER_SLIDER, "1,10")
         )),
 
         new Template("Propra1", "Übung_2", Arrays.asList(
-            new TemplateEntry("Was hast du heute gelernt?", TemplateEntry.AnswerType.TEXT, ""),
-            new TemplateEntry("Was hast du nicht verstanden?", TemplateEntry.AnswerType.TEXT, ""),
-            new TemplateEntry("Was könnte man besser machen?", TemplateEntry.AnswerType.MULTIPLE_CHOICE, "Mehr auf Schüler eingehen,Umfangreicher erklären,Weniger Hausaufgaben"),
-            new TemplateEntry("Schreibe eine Java-Klasse..", TemplateEntry.AnswerType.ATTACHEMENT, ".java"),
-            new TemplateEntry("Bewerte die Übung heute", TemplateEntry.AnswerType.NUMBER_SLIDER, "1,10")
+            new TemplateEntry("Was hast du heute gelernt?", AnswerType.TEXT, "Some hint"),
+            new TemplateEntry("Was hast du nicht verstanden?", AnswerType.TEXT, "Some hint"),
+            new TemplateEntry("Was könnte man besser machen?", AnswerType.MULTIPLE_CHOICE, "Mehr auf Schüler eingehen,Umfangreicher erklären,Weniger Hausaufgaben"),
+            new TemplateEntry("Schreibe eine Java-Klasse..", AnswerType.ATTACHEMENT, ".java"),
+            new TemplateEntry("Bewerte die Übung heute", AnswerType.NUMBER_SLIDER, "1,10")
         )),
 
         new Template("Aldat", "Vorlesung_1", Arrays.asList(
-            new TemplateEntry("Was hast du heute gelernt?", TemplateEntry.AnswerType.TEXT, ""),
-            new TemplateEntry("Was hast du nicht verstanden?", TemplateEntry.AnswerType.TEXT, ""),
-            new TemplateEntry("Was könnte man besser machen?", TemplateEntry.AnswerType.MULTIPLE_CHOICE, "Mehr auf Schüler eingehen,Umfangreicher erklären,Weniger Hausaufgaben"),
-            new TemplateEntry("Schreibe eine Java-Klasse..", TemplateEntry.AnswerType.ATTACHEMENT, ".java"),
-            new TemplateEntry("Bewerte die Übung heute", TemplateEntry.AnswerType.NUMBER_SLIDER, "1,10")
+            new TemplateEntry("Was hast du heute gelernt?", AnswerType.TEXT, "Some hint"),
+            new TemplateEntry("Was hast du nicht verstanden?", AnswerType.TEXT, "Some hint"),
+            new TemplateEntry("Was könnte man besser machen?", AnswerType.MULTIPLE_CHOICE, "Mehr auf Schüler eingehen,Umfangreicher erklären,Weniger Hausaufgaben"),
+            new TemplateEntry("Schreibe eine Java-Klasse..", AnswerType.ATTACHEMENT, ".java"),
+            new TemplateEntry("Bewerte die Übung heute", AnswerType.NUMBER_SLIDER, "1,10")
         ))
     );
 
