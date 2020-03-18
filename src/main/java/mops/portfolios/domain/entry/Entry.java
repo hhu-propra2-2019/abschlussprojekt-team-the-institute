78b2469e7f5270ff667a9ff6123ec0264c366fb5
Nisch206 <nicholas.schmitt.2212@googlemail.com>
2020-03-18 14:57:09 +0100

    Modifing the Domain/Entry files to fit google style.

--- src/main/java/mops/portfolios/domain/entry/Entry.java
+++ src/main/java/mops/portfolios/domain/entry/Entry.java
@@ -1,42 +1,45 @@
 package mops.portfolios.domain.entry;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.ArrayList;
-
-import javax.persistence.*;
-
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Entity
 @Data
 @RequiredArgsConstructor
 public class Entry {
-    private @Id @GeneratedValue @Getter Long id;
+  private @Id @GeneratedValue @Getter Long id;
 
-    private @Setter
-    @Column(nullable = false) String title;
+  private @Setter @Column(nullable = false) String title;
 
-    private final java.sql.Timestamp CreatedDate = new java.sql.Timestamp(Calendar.getInstance().getTime().getTime());
-    
-    private @LastModifiedDate Date LastModifiedDate;
+  private final java.sql.Timestamp createdDate = new java.sql.Timestamp(
+      Calendar.getInstance().getTime().getTime());
 
-    @OneToMany(
-        cascade = CascadeType.ALL,
-        fetch = FetchType.EAGER, //FIXME
-        orphanRemoval = true
-    )
-    private List<EntryField> fields = new ArrayList<>();
+  private @LastModifiedDate Date lastModifiedDate;
 
-    public Entry(String title) {
-        this.title = title;
-    }
+  @OneToMany(
+      cascade = CascadeType.ALL,
+      fetch = FetchType.EAGER, //FIXME
+      orphanRemoval = true
+  )
+  private List<EntryField> fields = new ArrayList<>();
+
+  public Entry(String title) {
+    this.title = title;
+  }
 
 }
