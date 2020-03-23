6e0288b17796e0c12494915b62fa68e8aa33f9d6
nickhoffmann7 <44677571+nickhoffmann7@users.noreply.github.com>
2020-03-23 13:27:27 +0100

    Implemented template creation and much more (#57)
    * improved range slider output
    
    * implemented template creation
    
    * added template entry and field creation
    
    * added fragments
    
    * fix pmd

--- src/main/java/mops/portfolios/controller/AdminController.java
+++ src/main/java/mops/portfolios/controller/AdminController.java
@@ -6,17 +6,26 @@ import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import lombok.AllArgsConstructor;
 import mops.portfolios.AccountService;
+import mops.portfolios.domain.entry.Entry;
+import mops.portfolios.domain.entry.EntryField;
+import mops.portfolios.domain.portfolio.Portfolio;
+import mops.portfolios.domain.portfolio.PortfolioService;
+import mops.portfolios.domain.portfolio.templates.AnswerType;
 import mops.portfolios.domain.portfolio.templates.Template;
+import mops.portfolios.domain.portfolio.templates.TemplateEntry;
 import mops.portfolios.domain.portfolio.templates.TemplateService;
+import mops.portfolios.domain.user.User;
 import mops.portfolios.tools.AsciiDocConverter;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin")
@@ -26,6 +35,7 @@ public class AdminController {
 
   private transient AccountService accountService;
 
+  private transient PortfolioService portfolioService;
   private transient TemplateService templateService;
 
   private transient AsciiDocConverter asciiConverter;
@@ -61,38 +71,6 @@ public class AdminController {
   }
 
   /**
-   * Create mapping for GET requests.
-   *
-   * @param model The spring model to add the attributes to
-   * @return The page to load
-   */
-  @GetMapping("/create")
-  public String createTemplate(Model model, KeycloakAuthenticationToken token) {
-    accountService.authorize(model, token);
-
-    return "admin/create";
-  }
-
-  /**
-   * Edit mapping for GET requests.
-   *
-   * @param model      The spring model to add the attributes to
-   * @param templateId The ID of the template
-   * @return The page to load
-   */
-  @GetMapping("/edit")
-  public String editTemplate(Model model, @RequestParam Long templateId,
-                             KeycloakAuthenticationToken token) {
-    accountService.authorize(model, token);
-
-    Template template = templateService.getById(templateId);
-
-    model.addAttribute("template", template);
-
-    return "admin/edit";
-  }
-
-  /**
    * View mapping for GET requests.
    *
    * @param model      The spring model to add the attributes to
@@ -100,13 +78,23 @@ public class AdminController {
    * @return The page to load
    */
   @GetMapping("/view")
-  public String viewTemplate(Model model, @RequestParam Long templateId,
-                             KeycloakAuthenticationToken token) {
+  public String viewTemplate(Model model, KeycloakAuthenticationToken token,
+                             @RequestParam Long templateId, @RequestParam(required = false) Long entryId) {
     accountService.authorize(model, token);
 
-    Template template = templateService.getById(templateId);
+    Portfolio portfolio = portfolioService.findPortfolioById(templateId);
 
+    Template template = templateService.convertPortfolioToTemplate(portfolio);
     model.addAttribute("template", template);
+
+    if (entryId == null && !portfolio.getEntries().isEmpty()) {
+      entryId = portfolio.getEntries().get(0).getId();
+    }
+
+    if (entryId != null) {
+      TemplateEntry templateEntry = templateService.getTemplateEntryById(template, entryId);
+      model.addAttribute("templateEntry", templateEntry);
+    }
 
     return "admin/view";
   }
@@ -152,5 +140,85 @@ public class AdminController {
     model.addAttribute("html", html);
 
     return "admin/asciidoc/view";
+  }
+
+  /**
+   * Create Template mapping for POST requests.
+   *
+   * @param model The spring model to add the attributes to
+   * @return The page to load
+   */
+  @PostMapping("/createTemplate")
+  public String createTemplate(Model model,
+                               KeycloakAuthenticationToken token, RedirectAttributes redirect,
+                               @RequestParam("title") String title) {
+    accountService.authorize(model, token);
+
+    User user = new User();
+    user.setName(token.getName());
+
+    Portfolio portfolio = new Portfolio(title, user);
+    portfolio.setTemplate(true);
+    portfolio = portfolioService.save(portfolio);
+
+    redirect.addAttribute("templateId", portfolio.getId());
+
+    return "redirect:/admin/view";
+  }
+
+
+  /**
+   * Create Template Entry mapping for POST requests.
+   *
+   * @param model The spring model to add the attributes to
+   * @return The page to load
+   */
+  @PostMapping("/createTemplateEntry")
+  public String createTemplateEntry(Model model,
+                                    KeycloakAuthenticationToken token, RedirectAttributes redirect,
+                                    @RequestParam Long templateId, @RequestParam("title") String title) {
+    accountService.authorize(model, token);
+
+    Portfolio portfolio = portfolioService.findPortfolioById(templateId);
+    Entry entry = new Entry(title);
+    portfolio.getEntries().add(entry);
+    portfolio = portfolioService.save(portfolio);
+
+    entry = portfolio.getEntries().get(portfolio.getEntries().size() - 1);
+
+    redirect.addAttribute("templateId", portfolio.getId());
+    redirect.addAttribute("entryId", entry.getId());
+
+    return "redirect:/admin/view";
+  }
+
+
+  /**
+   * Create Template Entry mapping for POST requests.
+   *
+   * @param model The spring model to add the attributes to
+   * @return The page to load
+   */
+  @PostMapping("/createTemplateField")
+  public String createTemplateField(Model model,
+                                    KeycloakAuthenticationToken token, RedirectAttributes redirect,
+                                    @RequestParam Long templateId, @RequestParam Long entryId,
+                                    @RequestParam("question") String question) {
+    accountService.authorize(model, token);
+
+    Portfolio portfolio = portfolioService.findPortfolioById(templateId);
+    Entry entry = portfolioService.findEntryById(portfolio, entryId);
+
+    EntryField field = new EntryField();
+    field.setTitle(question);
+    field.setContent(AnswerType.TEXT + ";Some hint");
+    entry.getFields().add(field);
+
+    portfolio = portfolioService.save(portfolio);
+
+    redirect.addAttribute("templateId", portfolio.getId());
+    redirect.addAttribute("entryId", entry.getId());
+
+    return "redirect:/admin/view";
   }
 }
--- src/main/java/mops/portfolios/domain/portfolio/templates/Template.java
+++ src/main/java/mops/portfolios/domain/portfolio/templates/Template.java
@@ -1,5 +1,6 @@
 package mops.portfolios.domain.portfolio.templates;
 
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -15,7 +16,11 @@ public class Template {
 
   private List<TemplateEntry> entries;
 
-  public TemplateEntry getLastEntry() {
-    return entries.get(entries.size() - 1);
+  public Date getLastChanged() {
+    if(!entries.isEmpty()) {
+      TemplateEntry lastEntry = entries.get(entries.size() - 1);
+      return lastEntry.getLastChanged();
+    }
+    return null;
   }
 }
