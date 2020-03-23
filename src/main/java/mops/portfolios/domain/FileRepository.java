54fcf20f62e196d88bb2c73eb37c7b6f2204f2aa
allap103 <allap103@hhu.de>
2020-03-23 14:26:21 +0100

    FileRepository für Nutzung mit Docker Container konfiguriert

--- src/main/java/mops/portfolios/domain/FileRepository.java
+++ src/main/java/mops/portfolios/domain/FileRepository.java
@@ -1,6 +1,13 @@
 package mops.portfolios.domain;
 
+import io.minio.MinioClient;
+import io.minio.errors.InvalidEndpointException;
+import io.minio.errors.InvalidPortException;
+
 public class FileRepository {
 
-    
+    MinioClient minioClient = new MinioClient("http://127.0.0.1:9000/", "minio", "minio123");
+
+    public FileRepository() throws InvalidPortException, InvalidEndpointException {
+    }
 }
