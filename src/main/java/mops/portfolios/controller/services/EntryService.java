dc7686210670efa6f60eb66840c01f6492f0ea25
mkasimd <49522425+mkasimd@users.noreply.github.com>
2020-03-24 15:56:44 +0100

    Refactoring: Fixed PMD & CheckStyle issues as well as added documentation
    Signed-off-by: mkasimd <49522425+mkasimd@users.noreply.github.com>

--- src/main/java/mops/portfolios/controller/services/EntryService.java
+++ src/main/java/mops/portfolios/controller/services/EntryService.java
@@ -1,14 +1,13 @@
 package mops.portfolios.controller.services;
 
+import java.util.Iterator;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import mops.portfolios.domain.entry.Entry;
 import mops.portfolios.domain.entry.EntryField;
 import mops.portfolios.domain.portfolio.Portfolio;
 import mops.portfolios.domain.portfolio.PortfolioService;
 import mops.portfolios.domain.portfolio.templates.AnswerType;
-
-import java.util.Iterator;
-import java.util.Set;
 
 // TODO: Eventuell mit entry.EntryService zusammenführen?
 
@@ -17,24 +16,36 @@ public class EntryService {
   private transient PortfolioService portfolioService;
 
 
+  /**
+   * Get the last entry in the provided Set.
+   * @param entries The Set of Entries
+   * @return the last entry in the provided Set
+   */
   @SuppressWarnings("PMD")
-  public  Entry getLast(Set<Entry> entries) {
+  public Entry getLast(Set<Entry> entries) {
     Iterator itr = entries.iterator();
     Entry last = (Entry)itr.next();
-    while(itr.hasNext()) {
+    while (itr.hasNext()) {
       last = (Entry)itr.next();
     }
     return last;
   }
 
 
+  /**
+   * Creates and adds an EntryField to an Entry.
+   * @param entryId the id of the entry the field is to be added
+   * @param question the question or title of the field to be added
+   * @param hint a hint
+   * @param portfolio the portfolio the entry belongs to
+   */
   public void createAndAdField(Long entryId, String question, String hint, Portfolio portfolio) {
     Entry entry = portfolioService.findEntryById(portfolio, entryId);
     EntryField field = new EntryField();
     entry.getFields().add(field);
 
     field.setTitle(question);
-    if(hint == null) {
+    if (hint == null) {
       hint = "Some hint";
     }
     field.setContent(AnswerType.TEXT + ";" + hint);
--- src/main/java/mops/portfolios/controller/services/FileService.java
+++ src/main/java/mops/portfolios/controller/services/FileService.java
@@ -1,28 +1,38 @@
 package mops.portfolios.controller.services;
 
+import java.io.IOException;
 import mops.portfolios.PortfoliosApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 @Service
 public class FileService {
-  private static transient final Logger logger = LoggerFactory.getLogger(PortfoliosApplication.class);
+  private static final transient Logger logger =
+          LoggerFactory.getLogger(PortfoliosApplication.class);
 
-  // TODO Extract into Service class
+  /**
+   * Checks whether anything is uploaded (whether file has any content).
+   * @param file The file to read
+   * @return <strong>true</strong> if no content or an error while parsing, \
+   * <strong>false</strong> if all's well
+   */
   public boolean nothingUploaded(MultipartFile file) {
     byte[] fileBytes = readFile(file);
-    if(fileBytes == null) {
+    if (fileBytes == null) {
       return true;
     }
 
     return false;
   }
 
-  // TODO: Extract into a Service
+  /**
+   * Reads and returns file as byte[].
+   * @param file the file to read
+   * @return <strong>null</strong> if any IOException while reading, the bytes of the file otherwise
+   */
+  @SuppressWarnings("PMD")
   public byte[] readFile(MultipartFile file) {
     byte[] fileBytes;
 
