5f2c50a36a91faf6852f0275dbdce9ea83e88eab
hanic101 <49514789+hanic101@users.noreply.github.com>
2020-03-24 15:28:38 +0100

    AsciiDocConverterTest added
    Signed-off-by: hanic101 <hannah-nickel@gmx.de>

---
+++ src/test/java/mops/portfolios/tools/AsciiDocConverterTest.java
@@ -0,0 +1,38 @@
+package mops.portfolios.tools;
+
+import lombok.NonNull;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class AsciiDocConverterTest {
+
+  private transient @NonNull AsciiDocConverter asciiDocConverter = new AsciiDocConverter();
+
+  @Test
+  void convertTest() {
+
+    String asciidoc = "+\n" +
+            "[quote,\"Charles Dickens\",\"A Tale of Two Cities\"]\n" +
+            "It was the best of times, it was the worst of times, it was the age of wisdom,\n" +
+            "it was the age of foolishness...";
+
+    String converted = asciiDocConverter.convertToHtml(asciidoc);
+    String expected = "<div class=\"paragraph\">\n" +
+            "<p>+</p>\n" +
+            "</div>\n" +
+            "<div class=\"quoteblock\">\n" +
+            "<blockquote>\n" +
+            "It was the best of times, it was the worst of times, it was the age of wisdom,\n" +
+            "it was the age of foolishness&#8230;&#8203;\n" +
+            "</blockquote>\n" +
+            "<div class=\"attribution\">\n" +
+            "&#8212; Charles Dickens<br>\n" +
+            "<cite>A Tale of Two Cities</cite>\n" +
+            "</div>\n" +
+            "</div>";
+
+    Assert.assertEquals(expected, converted);
+
+  }
+
+}
