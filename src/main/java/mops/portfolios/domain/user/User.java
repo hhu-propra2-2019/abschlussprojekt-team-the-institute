7ac3823e145e2d317a2ac9dd2a48a34c9b811546
allap103 <allap103@hhu.de>
2020-03-24 16:52:48 +0100

    Test geschrieben und Userklasse um notwendigen Konstruktor ergänzt

--- src/main/java/mops/portfolios/domain/user/User.java
+++ src/main/java/mops/portfolios/domain/user/User.java
@@ -2,6 +2,8 @@ package mops.portfolios.domain.user;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import mops.portfolios.domain.portfolio.Portfolio;
@@ -12,6 +14,7 @@ import javax.persistence.*;
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
   @Id String name;
   @OneToMany
--- src/test/java/mops/portfolios/UserServiceTest.java
+++ src/test/java/mops/portfolios/UserServiceTest.java
@@ -1,12 +1,18 @@
 package mops.portfolios;
 
 
+import mops.portfolios.domain.group.Group;
 import mops.portfolios.domain.group.GroupRepository;
+import mops.portfolios.domain.portfolio.Portfolio;
+import mops.portfolios.domain.user.User;
 import mops.portfolios.domain.user.UserRepository;
 import mops.portfolios.domain.user.UserService;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -14,21 +20,25 @@ import static org.mockito.Mockito.when;
 public class UserServiceTest {
 
     @Mock
-    GroupRepository groupRepositoryMock;
-    @Mock
     UserRepository userRepositoryMock;
-    UserService userService;
+    UserService userService = new UserService(userRepositoryMock);
 
     @Before
     public void setup() {
-        this.userService =
     }
 
 
     @Test
     public void userIsInGroupTest() {
-        when(groupRepositoryMock.)
+        List<Group> groupsOfTestUser = new ArrayList<>();
+        Group groupOfTestUser = new Group();
+        groupsOfTestUser.add(groupOfTestUser);
+        User testUser = new User("testUser", new ArrayList<Portfolio>(), groupsOfTestUser);
+        List<User> userInList = new ArrayList<>();
+        userInList.add(testUser);
+        when(userRepositoryMock.findAll()).thenReturn(userInList);
 
+        assert(userService.isUserNameInGroup("testUser", groupOfTestUser));
     }
     @Test
     public void userIsNotInGroupTest() {
