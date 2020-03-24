f5ff098c5de08254c91b5ee2acf82e9b9a104b31
allap103 <allap103@hhu.de>
2020-03-24 15:03:07 +0100

    Testklasse für UserService erstellt

---
+++ src/test/java/mops/portfolios/UserServiceTest.java
@@ -0,0 +1,29 @@
+package mops.portfolios;
+
+
+import org.junit.Before;
+
+public class UserServiceTest {
+
+
+
+    @Before
+    public void setup() {
+
+
+    }
+
+
+
+    public void userIsInGroupTest() {
+
+    }
+
+    public void userIsNotInGroupTest() {
+
+    }
+
+    public void userIsNotInEmptyGroupTest() {
+
+    }
+}
