1ad19122d147433f3fbf18161a5f294525d419a1
allap103 <allap103@hhu.de>
2020-03-25 17:40:39 +0100

    State Repository um Annotation für Autowiring ergänzt

--- src/main/java/mops/portfolios/domain/state/StateRepository.java
+++ src/main/java/mops/portfolios/domain/state/StateRepository.java
@@ -1,7 +1,9 @@
 package mops.portfolios.domain.state;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StateRepository extends CrudRepository<State, String> {
 }
 
