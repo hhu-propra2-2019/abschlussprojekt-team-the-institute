342a2a50556dd0575ea65fe2fd17c78cf85e5570
hanic101 <hannah-nickel@gmx.de>
2020-03-26 17:58:42 +0100

    CheckStyle in GroupService

--- src/main/java/mops/portfolios/domain/group/GroupService.java
+++ src/main/java/mops/portfolios/domain/group/GroupService.java
@@ -1,33 +1,40 @@
 package mops.portfolios.domain.group;
 
+import java.util.List;
 import mops.portfolios.domain.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class GroupService {
 
-    @Autowired
-    transient GroupRepository repository;
+  @Autowired
+  transient GroupRepository repository;
 
-    public Group getGroup(Long groupId) {
-        return repository.findById(groupId).get();
-    }
+  public Group getGroup(Long groupId) {
+    return repository.findById(groupId).get();
+  }
 
-    public List<User> getUsers(Long groupId) {
-        Group group = repository.findById(groupId).get();
+  /**
+   * Gets list of users by groupID.
+   * @return list of users
+   */
+  public List<User> getUsers(Long groupId) {
+    Group group = repository.findById(groupId).get();
 
-        return group.getUsers();
-    }
+    return group.getUsers();
+  }
 
-    public void updateGroup(Long groupId, List<User> users) {
+  /**
+   * Updates group.
+   */
+  public void updateGroup(Long groupId, List<User> users) {
 
-        Group group = repository.findById(groupId).get();
+    Group group = repository.findById(groupId).get();
 
-        group.setUsers(users);
+    group.setUsers(users);
 
-        repository.save(group);
-    }
+    repository.save(group);
+  }
 }
