44a3897ae72b9caffe1bfc94bcded3008732b5c8
hanic101 <hannah-nickel@gmx.de>
2020-03-26 18:08:49 +0100

    CheckStyle in EntryField

--- src/main/java/mops/portfolios/domain/entry/EntryField.java
+++ src/main/java/mops/portfolios/domain/entry/EntryField.java
@@ -3,10 +3,7 @@ package mops.portfolios.domain.entry;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
 import lombok.*;
-import mops.portfolios.domain.FileRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Entity
 @Data
