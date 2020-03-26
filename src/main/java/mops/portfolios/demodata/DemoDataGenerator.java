3fab8ea454494bdce2466f36bb4524268d9ff7dc
Nisch206 <nicholas.schmitt.2212@googlemail.com>
2020-03-26 13:46:26 +0100

    Working Forms

--- src/main/java/mops/portfolios/demodata/DemoDataGenerator.java
+++ src/main/java/mops/portfolios/demodata/DemoDataGenerator.java
@@ -135,7 +135,7 @@ public class DemoDataGenerator {
       AnswerType.TEXT.name() + ";Some hint",
       AnswerType.SINGLE_CHOICE.name() + ";Ja,Nein",
       AnswerType.MULTIPLE_CHOICE.name() + ";Mehr auf Schüler eingehen,Umfangreicher erklären,Weniger Hausaufgaben",
-      AnswerType.NUMBER_SLIDER.name() + ";1,10",
+      AnswerType.NUMBER_SLIDER.name() + ";1,10,1",
       AnswerType.ATTACHEMENT.name() + ";.ascii,.pdf,.java"
   );
   private transient List<String> templateEntryTitles = Arrays.asList(
