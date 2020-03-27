760b0efcd567868ec4bb7f66b1e1e0cbe59a471d
hanic101 <hannah-nickel@gmx.de>
2020-03-27 13:07:47 +0100

    Fixing failing tests

--- src/test/java/mops/portfolios/controller/services/FileServiceTest.java
+++ src/test/java/mops/portfolios/controller/services/FileServiceTest.java
@@ -1,20 +1,27 @@
 package mops.portfolios.controller.services;
 
+import lombok.NonNull;
+import mops.portfolios.domain.file.FileRepository;
 import mops.portfolios.domain.file.FileService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
+import static org.mockito.Mockito.mock;
 
 public class FileServiceTest {
 
   private transient MultipartFile multipartFile = new MockMultipartFile("data", "filename.txt", "text/plain", "some xml".getBytes());
-  private transient FileService fileService;
+  private transient FileRepository fileRepository = mock(FileRepository.class);
+
+  @NonNull
+  private transient FileService fileService = new FileService(fileRepository);
 
 
 
   @Test
   public void nothingUploadedTest() {
+
     boolean nothingUploaded = fileService.nothingUploaded(multipartFile);
 
     Assert.assertEquals(false, nothingUploaded);
