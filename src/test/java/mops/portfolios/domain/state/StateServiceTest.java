de42285883d222feafe01b03a6c8008d43a8f416
hanic101 <hannah-nickel@gmx.de>
2020-03-27 16:09:02 +0100

    Fixing StateServiceTest

--- src/test/java/mops/portfolios/domain/state/StateServiceTest.java
+++ src/test/java/mops/portfolios/domain/state/StateServiceTest.java
@@ -4,31 +4,36 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class StateServiceTest {
 
+  private transient StateRepository stateRepository = mock(StateRepository.class);
   @Autowired
-  private transient StateService stateService;
+  private transient StateService stateService = new StateService(stateRepository);
 
   @Test
   public void getStateTest() {
+
+    State state = new State();
+    state.setLastState(7257L);
+    state.setId("test");
+
+    when(stateRepository.save(any(State.class))).thenReturn(state);
+
     stateService.setState("test", 7257L);
+
+    when(stateRepository.findById("test")).thenReturn(Optional.of(state));
 
     Long stateFromRepo = stateService.getState("test");
 
     assert(stateFromRepo == 7257L);
-  }
-
-  @Test
-  public void getStateFromEmpty() {
-
-    Long uninitializedState = stateService.getState("uninitializedState");
-
-    assert (uninitializedState == 0L);
-
   }
 
 }
