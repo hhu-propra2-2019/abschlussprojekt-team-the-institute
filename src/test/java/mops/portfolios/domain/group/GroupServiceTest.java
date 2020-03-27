4231e871e11056bd3ea0fd4636e3a0ccb6c7e866
hanic101 <hannah-nickel@gmx.de>
2020-03-27 15:54:40 +0100

    Fixing PMD

--- src/test/java/mops/portfolios/domain/group/GroupServiceTest.java
+++ src/test/java/mops/portfolios/domain/group/GroupServiceTest.java
@@ -2,9 +2,6 @@ package mops.portfolios.domain.group;
 
 
 import lombok.NonNull;
-import mops.portfolios.domain.group.Group;
-import mops.portfolios.domain.group.GroupRepository;
-import mops.portfolios.domain.group.GroupService;
 import mops.portfolios.domain.user.User;
 import mops.portfolios.domain.user.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -21,37 +18,37 @@ import static org.mockito.Mockito.when;
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class GroupServiceTest {
 
-    @NonNull @Autowired
-    private transient UserRepository userRepository = mock(UserRepository.class);
-    @NonNull @Autowired
-    private transient GroupRepository groupRepository = mock(GroupRepository.class);
-    @NonNull @Autowired
-    private transient GroupService groupService = new GroupService(groupRepository);
+  @NonNull @Autowired
+  private transient UserRepository userRepository = mock(UserRepository.class);
+  @NonNull @Autowired
+  private transient GroupRepository groupRepository = mock(GroupRepository.class);
+  @NonNull @Autowired
+  private transient GroupService groupService = new GroupService(groupRepository);
 
-    @Test
-    public void testTest() {
-        assert(true);
-    }
-    @Test
-    public void getUsersTest() {
-        User user1 = new User("testUser", new ArrayList<>(), new ArrayList<>());
+  @Test
+  public void testTest() {
+    assert(true);
+  }
+  @Test
+  public void getUsersTest() {
+    User user1 = new User("testUser", new ArrayList<>(), new ArrayList<>());
 
-        when(userRepository.save(any(User.class))).thenReturn(user1);
-        userRepository.save(user1);
+    when(userRepository.save(any(User.class))).thenReturn(user1);
+    userRepository.save(user1);
 
-        List<User> userList = new ArrayList<>();
-        userList.add(user1);
+    List<User> userList = new ArrayList<>();
+    userList.add(user1);
 
-        Group testGroup = new Group(0L, "testGroup", userList);
+    Group testGroup = new Group(0L, "testGroup", userList);
 
-        when(groupRepository.save(any(Group.class))).thenReturn(testGroup);
-        groupService.saveGroup(testGroup);
+    when(groupRepository.save(any(Group.class))).thenReturn(testGroup);
+    groupService.saveGroup(testGroup);
 
-        when(groupRepository.findById(0L)).thenReturn(Optional.of(testGroup));
+    when(groupRepository.findById(0L)).thenReturn(Optional.of(testGroup));
 
-        List<User> userListFromDatabase = groupService.getUsers(0L);
-        System.out.println(userListFromDatabase.toString());
+    List<User> userListFromDatabase = groupService.getUsers(0L);
+    System.out.println(userListFromDatabase.toString());
 
-        assert(userListFromDatabase.containsAll(userList));
-    }
+    assert(userListFromDatabase.containsAll(userList));
+  }
 }
--- src/test/java/mops/portfolios/domain/user/UserServiceTest.java
+++ src/test/java/mops/portfolios/domain/user/UserServiceTest.java
@@ -3,47 +3,42 @@ package mops.portfolios.domain.user;
 import mops.portfolios.domain.group.Group;
 
 import mops.portfolios.domain.portfolio.Portfolio;
-import mops.portfolios.domain.user.User;
-import mops.portfolios.domain.user.UserRepository;
-import mops.portfolios.domain.user.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class UserServiceTest {
 
-    @Mock
-    UserRepository userRepositoryMock = mock(UserRepository.class);
-    UserService userService = new UserService(userRepositoryMock);
+  @Mock
+  private transient UserRepository userRepositoryMock = mock(UserRepository.class);
+  private transient UserService userService = new UserService(userRepositoryMock);
 
 
-    @Test
-    public void userIsInGroupTest() {
-        Group groupOfTestUser = new Group();
-        List<Group> groupsOfTestUser = new ArrayList<>();
-        groupsOfTestUser.add(groupOfTestUser);
-        User testUser = new User("testUser", new ArrayList<>(), groupsOfTestUser);
-        when(userRepositoryMock.findById("testUser")).thenReturn(Optional.of(testUser));
+  @Test
+  public void userIsInGroupTest() {
+    Group groupOfTestUser = new Group();
+    List<Group> groupsOfTestUser = new ArrayList<>();
+    groupsOfTestUser.add(groupOfTestUser);
+    User testUser = new User("testUser", new ArrayList<>(), groupsOfTestUser);
+    when(userRepositoryMock.findById("testUser")).thenReturn(Optional.of(testUser));
 
-        assert(userService.isUserNameInGroup("testUser", groupOfTestUser));
-    }
+    assert(userService.isUserNameInGroup("testUser", groupOfTestUser));
+  }
 
 
-    @Test
-    public void userIsNotInGroupTest() {
-        Group group = new Group();
-        User testUser = new User("testUser", new ArrayList<Portfolio>(), new ArrayList<Group>());
-        List<User> userInList = new ArrayList<>();
-        userInList.add(testUser);
-        when(userRepositoryMock.findById("testUser")).thenReturn(Optional.of(testUser));
+  @Test
+  public void userIsNotInGroupTest() {
+    Group group = new Group();
+    User testUser = new User("testUser", new ArrayList<Portfolio>(), new ArrayList<Group>());
+    List<User> userInList = new ArrayList<>();
+    userInList.add(testUser);
+    when(userRepositoryMock.findById("testUser")).thenReturn(Optional.of(testUser));
 
-        assertFalse(userService.isUserNameInGroup("testUser", group));
-    }
+    assertFalse(userService.isUserNameInGroup("testUser", group));
+  }
 }
